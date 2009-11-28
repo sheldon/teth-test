78d177c1fef58032eccd5d6d61f83369988ba631
Sheldon <sheldon.els@gmail.com>
2009-11-28 14:27:02 +0000

    first test checked in for path_to function in autoloader

--- tests/TestAutoloader.php
+++ tests/TestAutoloader.php
@@ -4,7 +4,28 @@ class TestAutoloader extends BaseTest{
   public $class_path = "../../teth/Autoloader.php";
   
   public function path_to(){
-    print_r(Autoloader::path_to('autoloader')); exit;
+    $ret = true;
+    
+    //normal syntax usage
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass');
+    if(Autoloader::path_to('foo_index') != "FRAMEWORK_DIRFooClass.php") $ret = false;
+    
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass', 'component'=>'FooComponent');
+    if(Autoloader::path_to('foo_index') != "FRAMEWORK_DIRFooComponent/FooClass.php") $ret = false;
+    
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass', 'module'=>'FooModule');
+    if(Autoloader::path_to('foo_index') != "FRAMEWORK_DIRFooModule/FooClass.php") $ret = false;
+    
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass', 'component'=>'FooComponent', 'module'=>'FooModule');
+    if(Autoloader::path_to('foo_index') != "FRAMEWORK_DIRFooComponent/FooModule/FooClass.php") $ret = false;
+    
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass', 'component'=>'FooComponent', 'module'=>'FooModule', 'suffix'=>'.test');
+    if(Autoloader::path_to('foo_index') != "FRAMEWORK_DIRFooComponent/FooModule/FooClass.test") $ret = false;
+    
+    Config::$settings['classes']['foo_index'] = array('class'=>'FooClass', 'component'=>'FooComponent', 'module'=>'FooModule', 'base'=>'/a/file/path/instead/of/a/constant/');
+    if(Autoloader::path_to('foo_index') != "/a/file/path/instead/of/a/constant/FooComponent/FooModule/FooClass.php") $ret = false;
+    
+    return $ret;
   }
   public function class_for(){}
   public function class_in_config(){}
