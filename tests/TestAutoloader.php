2b8c5c1e1a99ca832df4203eb0e1b860b1e20699
Sheldon <sheldon.els@gmail.com>
2009-11-28 00:43:29 +0000

    initial attempt to write a test. fail. not sure why, but the include breaks on __autoload, will investigate.

--- tests/TestAutoloader.php
+++ tests/TestAutoloader.php
@@ -1,6 +1,12 @@
 <?php
 class TestAutoloader extends BaseTest{
-  public function path_to(){}
+  public function __construct(){
+    include("../../teth/Autoloader.php");
+    $this->class = new Autoloader();
+  }
+  public function path_to(){
+    print_r(Autoloader::path_to('autoloader')); exit;
+  }
   public function class_for(){}
   public function class_in_config(){}
   public function pre_init_hooks(){}
