d832c95db26f8085c6bdc06f6b5514bf32d04dd9
Sheldon <sheldon.els@gmail.com>
2009-12-10 00:38:20 +0000

    no need for a special file to handle command line, put that functionality into TestRunner

--- run.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-include("TestRunner.php");
-$test_runner = new TestRunner();
-$test_runner->scan_folders[] = "tests";
-$test_runner->scan();
-echo($test_runner->run_tests());
-?>
