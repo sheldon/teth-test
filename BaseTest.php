e8ce93375fcf109f297727f4672f7980bec2478f
Sheldon <sheldon.els@gmail.com>
2009-12-10 00:39:56 +0000

    base test class, ability to run it's own tests and return state about them

--- BaseTest.php
+++ BaseTest.php
@@ -1,3 +1,26 @@
 <?php
 class BaseTest{
+  public $class_path = false;
+  public $class = false;
+  public $output = "";
+  public $tests_passed = 0;
+  public $tests_failed = 0;
+
+  public function run_tests() {
+    if($this->class_path) include_once($this->class_path);
+    if($this->class) $this->class = new $this->class;
+    foreach(array_diff(get_class_methods(get_class($this)),array("run_tests")) as $test){
+      $test_class = get_class($this);
+      $test_object = new $test_class;
+      $this->output .= "  Running $test ... ";
+      if($test_object->$test()){
+        $this->tests_passed++;
+        $this->output .= "pass\n";
+      }else{
+        $this->tests_failed++;
+        $this->output .= "fail\n";
+      }
+    }
+    return $this;
+  }
 }?>
